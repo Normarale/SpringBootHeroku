c68c78cbdcf5d09a230d02500051647fa902cd03
Normis <normadelprato@gmail.com>
2022-05-20 17:49:10 -0300

    BackendPortfolio

--- src/main/java/com/desarrollo/portfolio/PortfolioApplication.java
+++ src/main/java/com/desarrollo/portfolio/PortfolioApplication.java
@@ -15,7 +15,7 @@ public class PortfolioApplication {
 
                
         @Bean
-	public WebMvcConfigurer corsConfigurer() {
+        public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
